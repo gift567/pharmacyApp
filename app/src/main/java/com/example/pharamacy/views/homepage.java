ea05b7fa1b8c820a4d35d768082d0fa7df9ca975
Gift kateule <giftkateule719@gmail.com>
2022-05-03 02:40:11 -0700

    the cart has failed

--- app/src/main/java/com/example/pharamacy/views/homepage.java
+++ app/src/main/java/com/example/pharamacy/views/homepage.java
@@ -1,5 +1,6 @@
 package com.example.pharamacy.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -22,6 +24,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class homepage extends AppCompatActivity {
 
 
+   NavController navController;
+
 
     RecyclerView featuredRecycler;
     private FirebaseAuth mAuth;
@@ -31,7 +35,10 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+
 
 
 
@@ -65,5 +72,11 @@ mAuth = FirebaseAuth.getInstance();
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        NavigationUI.onNavDestinationSelected(item, navController);
+        return super.onOptionsItemSelected(item);
     }
 }
